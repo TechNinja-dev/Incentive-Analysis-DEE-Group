bb9dbf7b6bd48773ba6a493ec1be145b350d518d
Prakhar Srivastava <prakharsrivastava019@gmail.com>
2025-12-19 15:18:43 +0530

    dashboard completed

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List,Optional
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+class CarData(BaseModel):
+    name: str
+    quantity: int
+    min_slab: int
+    total_amount: int
+
+class IncentiveRequest(BaseModel):
+    target: int
+    tenure: Optional[int] = None
+    cars: List[CarData]
+
+
+
+@app.post("/incentives")
+def receive_incentives(data: IncentiveRequest):
+    print(data)
