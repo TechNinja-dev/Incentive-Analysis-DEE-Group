76390f1cfd4b3842d002e6f5d42bde685efaaa55
Prakhar Srivastava <prakharsrivastava019@gmail.com>
2025-12-24 10:21:10 +0530

    Encryption updated

--- backend/encryption.py
+++ backend/encryption.py
@@ -1,18 +1,15 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def normalize_password(password: str) -> bytes:
-    # normalize → encode → truncate BYTES
-    return password.strip().encode("utf-8")[:72]
-
+pwd_context = CryptContext(
+    schemes=["bcrypt_sha256"],
+    deprecated="auto"
+)
 
 def hash_password(password: str) -> str:
-    password = normalize_password(password)
     return pwd_context.hash(password)
 
 
 def verify_password(password: str, hashed_password: str) -> bool:
-    password = normalize_password(password)
     return pwd_context.verify(password, hashed_password)
+
 
